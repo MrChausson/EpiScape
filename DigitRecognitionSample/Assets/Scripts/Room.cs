73684d622ba9a1393144e746201a30d60b64c17a
Grégoire Biendiné <60387414+gregoirebiendine@users.noreply.github.com>
2024-04-03 14:03:21 +0200

    update episcape scene by adding a second room

--- DigitRecognitionSample/Assets/Scripts/Room.cs
+++ DigitRecognitionSample/Assets/Scripts/Room.cs
@@ -21,8 +21,10 @@ public class Room : MonoBehaviour
     public bool fruitCodes = false; // specific code for fruit level
     public int[] code; // secret code
     public int codePosition = 0; //e.g. is it the first, second or third digit you are entering
+
     public enum DOOR_STATE { OPEN, CLOSED, OPENING, CLOSING };
     public DOOR_STATE doorState = DOOR_STATE.CLOSED;
+
 
     // supported digits
     int[] numbers = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
